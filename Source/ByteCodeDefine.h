bcd00ebb174fb5725eb1b4a0f4d65257702469cd
松田昊 <240343@jc-21.jp>
2025-06-24 18:18:28 +0900

    ✨バイトコード追加

--- Source/ByteCodeDefine.h
+++ Source/ByteCodeDefine.h
@@ -4,12 +4,28 @@ using Byte = unsigned char;
 
 enum ByteCodeDefine : Byte
 {
-	BCD_NOP = 0x00,  // 何もしない
-	// ...
-	BCD_HALT = 0x76,  // 停止
-	// ...
-	BCD_ACT = 0xCB,  // アクション
-	// ...
+	// 何もしない
+	BCD_NOP = 0x00,  // ()
+	// 停止
+	BCD_HALT = 0x76,  // ()
+	// アクションする
+	BCD_ACT = 0xCB,  // (アクション番号)
+	
+	// 関数呼び出し
+	BCD_CALL = 0xCD,  // (関数のポインタ)
+	// 関数返却
+	BCD_RET = 0xC9,  // () スタックマシンに返却値を入れる
+	// ジャンプ
+	BCD_JP = 0xC3,  // ()
+
+	// スタックマシンにプッシュする
+	BCD_PUSH = 0xC5,  // (アドレス)
+
+	// 
+	BCD_ADD = 0x6B,
+
+	// スタックマシンにポップする
+	BCD_POP = 0xC1,  // (代入先アドレス)
 };
 
 enum ByteCodeDefine_ACT : Byte
